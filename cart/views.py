3229f954c20e47c4f8e6fde403172e6ed5c284b0
Pac_Man <pac_man@gmail.com>
2024-07-17 20:17:24 +0500

    new

--- cart/views.py
+++ cart/views.py
@@ -36,23 +36,22 @@ class CartListAPIView(generics.ListAPIView):
 
 class CartPlusAPIView(APIView):
     permission_classes = [IsAuthenticated, IsOwner]
-    serializer_class = CartAddSerializer
 
     def put(self, request, pk):
-        print(pk)
-        post = CartModel.objects.filter(product_id_id=pk).first( )
-        print(post)
-        if not post.exists():
+        try:
+            post = CartModel.objects.get(product_id=pk, user_id=request.user)
+        except CartModel.DoesNotExist:
             response = {
-                "status": True,
-                "message": "does not found",
+                "status": False,
+                "message": "Product not found in cart.",
             }
             return Response(response, status=status.HTTP_404_NOT_FOUND)
 
-        serializer = CartModel.objects.filter(product_id=pk)
+        serializer = CartAddSerializer(post, data=request.data, partial=True)
+
         if serializer.is_valid():
-            self.check_object_permissions(post, request)
-            serializer.save(post.quantity + 1)
+            post.quantity += 1
+            post.save()
             response = {
                 "status": True,
                 "message": "Successfully updated"
